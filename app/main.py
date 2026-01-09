16fed4f2d93762f65d1038aa513f9c6c990a0ffa
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2026-01-09 12:34:15 -0500

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
+import os
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self):
+        # Nothing special to return
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Remove the file if it exists when exiting the context
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
